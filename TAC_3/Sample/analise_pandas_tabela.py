e3b5077860804a7ac45c02aecf94c301f49010d6
julia <juliarodriguesfrz@gmail.com>
2021-02-21 20:38:32 -0300

    Funções usando pandas

--- TAC_3/Sample/analise_pandas_tabela.py
+++ TAC_3/Sample/analise_pandas_tabela.py
@@ -13,15 +13,15 @@ from estatisticas_pandas_script import *
 def main():
     nome_do_arquivo = sys.argv[1]
 
-    coluna_min = input("Digite o nome da coluna que deseja calcular o valor mínimo: ")
+    coluna_min = input("Digite o nome da coluna que você deseja calcular o valor mínimo: ")
     valor_min(nome_do_arquivo, coluna_min)
     coluna_media = input("Digite o nome da coluna que deseja calcular a média: ")
     media(nome_do_arquivo, coluna_media)
     coluna_max = input("Digite o nome da coluna que deseja calcular o valor máximo: ")
     valor_max(nome_do_arquivo, coluna_max)
-    soma_total_coluna = input("Digite o nome da coluna que você deseja calcular a média: ")
+    soma_total_coluna = input("Digite o nome da coluna que você deseja calcular a soma: ")
     soma_total(nome_do_arquivo, soma_total_coluna)
-    valor_de_x = input("Digite a quantidade de países com maior número de casos que você deseja visualizar: ")
+    valor_de_x = int(input("Digite a quantidade de países com maior número de casos que você deseja visualizar: "))
     topx(nome_do_arquivo, valor_de_x)
     cria_coluna(nome_do_arquivo)
 
--- TAC_3/Sample/estatisticas_pandas_script.py
+++ TAC_3/Sample/estatisticas_pandas_script.py
@@ -13,24 +13,24 @@ def valor_max(filename:str, coluna:str):
     arquivo_excel = pandas.read_excel(filename)
     coluna_max = arquivo_excel[coluna]
     max_value = coluna_max.max()
-    print(max_value)
+    print(max_value,'\t\t')
 def valor_min(filename:str, coluna:str):
     arquivo_excel = pandas.read_excel(filename)
     coluna_min = arquivo_excel[coluna]
     min_value = coluna_min.min()
-    print(min_value)
+    print(min_value,'\t\t')
     
 def media(filename:str, coluna:str):
     arquivo_excel = pandas.read_excel(filename)
     coluna_media = arquivo_excel[coluna]
     media = coluna_media.mean()
-    print("Média:",media)
+    print("Média:", media,'\t\t')
 
 def soma_total(filename:str, coluna:str):
     arquivo_excel = pandas.read_excel(filename)
     coluna_soma = arquivo_excel[coluna]
     soma_coluna = coluna_soma.sum()
-    print(soma_coluna)
+    print(soma_coluna,'\t\t')
 
 def topx(filename:str, x: int):
     arquivo_excel = pandas.read_excel(filename)
@@ -38,7 +38,7 @@ def topx(filename:str, x: int):
     # printar sem o index (primeira coluna que indica os números das linhas).
     # a função .to_string transforma a váriavel tabular em strings e retira a indexação.
     #print(ordem_decrescente_x.to_string(index=False))
-    print(ordem_decrescente_x[0:x])
+    print(ordem_decrescente_x.iloc[:x].to_string(index = False),'\t\t')
 
 def cria_coluna(filename):
     # normalização por 100.000 habitantes.
@@ -46,5 +46,5 @@ def cria_coluna(filename):
     pop_normalizada = arquivo_excel['Population'] / 100
     # criar coluna chamada “Total_cases_per_100mil”
     arquivo_excel['Total_cases_per_100mil'] = arquivo_excel['Total_deaths'] / pop_normalizada
-    print('Quantidade de casos por 100.000 habitantes:')
-    print(arquivo_excel)
+    print('Quantidade de casos por 100.000 habitantes:\t')
+    print(arquivo_excel.to_string(index = False))
