cf2b4cf3934b5a9693ed1efb050e4465299d7406
julia <juliarodriguesfrz@gmail.com>
2021-02-21 16:51:08 -0300

    Função usando pandas

--- TAC_3/Sample/analise_pandas_tabela.py
+++ TAC_3/Sample/analise_pandas_tabela.py
@@ -13,16 +13,16 @@ from estatisticas_pandas_script import *
 def main():
     nome_do_arquivo = sys.argv[0]
 
-    coluna_min = int(input("Digite o número da coluna que deseja calcular o valor mínimo: "))
+    coluna_min = input("Digite o nome da coluna que deseja calcular o valor mínimo: ")
     valor_min(nome_do_arquivo, coluna_min)
-    coluna_media = int(input("Digite o número da coluna que deseja calcular a média: "))
+    coluna_media = input("Digite o nome da coluna que deseja calcular a média: ")
     media(nome_do_arquivo, coluna_media)
-    coluna_max = int(input("Digite o número da coluna que deseja calcular o valor máximo: "))
+    coluna_max = input("Digite o nome da coluna que deseja calcular o valor máximo: ")
     valor_max(nome_do_arquivo, coluna_max)
-    soma_total_coluna = int(input("Digite o número da coluna que você deseja calcular a média "))
+    soma_total_coluna = input("Digite o nome da coluna que você deseja calcular a média ")
     soma_total(nome_do_arquivo, soma_total_coluna)
-    coluna_e_x = input("Digite o numero da coluna que voce deseja e o valor de x")
-    topx(nome_do_arquivo, int(coluna_e_x[2]),(coluna_e_x[1]))
+    coluna_e_x = input("Digite o nome da coluna que voce deseja e o valor de x")
+    topx(nome_do_arquivo, (coluna_e_x[2]),(coluna_e_x[1]))
     cria_coluna(nome_do_arquivo)
 
 if __name__ == "__main__":
